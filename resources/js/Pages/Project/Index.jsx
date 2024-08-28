6ecfff08be41253430d9122eaf9e1d70ab9a596c
Charanjeet Singh <charanjeet@majesticinformatics.com>
2024-08-28 20:35:56 +0530

    Closing tag was missing

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -17,7 +17,7 @@ export default function Index({ auth, projects }) {
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 text-gray-900">
-                <tableb className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                         <tr className="text-nowrap">
                             <th className="px-3 py-2">ID</th>
